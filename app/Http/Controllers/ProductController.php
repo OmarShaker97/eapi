86dcc16e41e89fc02e12f82aaf2d4a73976ab586
OmarShaker97 <mrnickgage@gmail.com>
2020-10-06 14:58:44 +0200

    Product Resource created

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return Product::all();
     }
 
     /**
@@ -47,6 +47,8 @@ class ProductController extends Controller
     public function show(Product $product)
     {
         //
+
+        return new ProductResource($product);
     }
 
     /**
